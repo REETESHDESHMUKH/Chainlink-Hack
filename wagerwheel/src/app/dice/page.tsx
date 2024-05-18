85cfd39a92b70c5cef4f574c65796e87b02fd7fd
REETESH DESHMUKH <reeteshdeshmukhbpl@gmail.com>
2024-05-18 12:01:30 +0530

    create all 3 games folder

---
+++ wagerwheel/src/app/dice/page.tsx
@@ -0,0 +1,7 @@
+export default function Dice() {
+    return (
+        <div>
+            dice!
+        </div>
+    )
+}
---
+++ wagerwheel/src/app/mines/page.tsx
@@ -0,0 +1,7 @@
+export default function Mines() {
+    return (
+        <div>
+            mines!
+        </div>
+    )
+}
